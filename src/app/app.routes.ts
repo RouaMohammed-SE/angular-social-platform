264e080e14f21fdf564bb4dedd8602942d8a8e7a
roaamuhamed <roaamuhamed312@gmail.com>
2026-03-02 05:28:49 +0200

    feat(auth): implement auth layout structure with routing tabs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,64 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './features/auth/components/login/login.component';
+import { RegisterComponent } from './features/auth/components/register/register.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { FeedComponent } from './features/feed/feed.component';
+import { ProfileComponent } from './features/profile/profile.component';
+import { NotificationsComponent } from './features/notifications/notifications.component';
+import { ChangePasswordComponent } from './features/change-password/change-password.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Sign In | Route Posts',
+      },
+      { path: 'register', component: RegisterComponent, title: 'Create Account | Route Posts' },
+    ],
+  },
+
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'feed',
+        component: FeedComponent,
+        title: 'Home Feed | Route Posts',
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+        title: 'Profile | Route Posts',
+      },
+      {
+        path: 'notifications',
+        component: NotificationsComponent,
+        title: 'Notifications | Route Posts',
+      },
+      {
+        path: 'changePassword',
+        component: ChangePasswordComponent,
+        title: 'Change Password | Route Posts',
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: 'Page Not Found | Route Posts',
+  },
+];
